071df28c4c6f5d611e981fded27bd01e377f4ec5
1nv8rZim <msf9542@rit.edu>
2023-06-05 21:23:23 -0400

    Better version output

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
 BINARY_NAME=compscore
 OUT_FILE=build/$(BINARY_NAME)
 
+GIT_COMMIT=$(shell git rev-parse HEAD)
+GIT_BRANCH=$(shell git rev-parse --abbrev-ref HEAD)
+BUILD_DATE=$(shell date +%Y-%m-%d\ %H:%M)
+
+LDFLAGS=-ldflags "-X 'main.gitCommit=${GIT_COMMIT}' -X 'main.gitBranch=${GIT_BRANCH}' -X 'main.buildDate=${BUILD_DATE}'"
+
 .PHONY: build clean start status stop kill
 
 build:
-	go build -o $(OUT_FILE) main.go
+	go build $(LDFLAGS) -o $(OUT_FILE) main.go
 
 clean:
 	rm -f $(OUT_FILE)
--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"os/exec"
 	"runtime"
-	"strings"
 
 	"github.com/compscore/compscore/cmd"
 	"github.com/spf13/cobra"
@@ -17,6 +15,10 @@ func main() {
 
 //go:embed version
 var version string
+
+var gitCommit string
+var gitBranch string
+var buildDate string
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -31,19 +33,9 @@ func init() {
 
 func versionCmdRun(cmd *cobra.Command, args []string) {
 	fmt.Println("Version:", version)
-	fmt.Println("Git Commit:", getGitCommit())
+	fmt.Println("Git Commit:", gitCommit)
+	fmt.Println("Git Branch:", gitBranch)
+	fmt.Println("Build Date:", buildDate)
 	fmt.Println("Go Version:", runtime.Version())
 	fmt.Println("OS/Arch:", runtime.GOOS+"/"+runtime.GOARCH)
-}
-
-func getGitCommit() string {
-	cmd := exec.Command("git", "rev-parse", "HEAD")
-	output, err := cmd.Output()
-
-	if err != nil {
-		fmt.Println("Error getting git commit:", err)
-		return ""
-	}
-
-	return strings.TrimSpace(string(output))
 }
