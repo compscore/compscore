5e6042e9de79051de70e1785e01619bbd7dbe9bb
1nv8rZim <msf9542@rit.edu>
2023-08-28 16:54:57 -0400

    implemenation of running config

--- pkg/grpc/server/getConfig.go
+++ pkg/grpc/server/getConfig.go
@@ -1,10 +1,7 @@
 package server
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"strings"
 
 	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/grpc/proto"
@@ -15,41 +12,10 @@ import (
 func (*compscoreServer_s) GetConfig(ctx context.Context, in *proto.GetConfigRequest) (*proto.GetConfigResponse, error) {
 	logrus.Info("Received get config request")
 
-	heading := strings.Repeat("=", 20) + "\n"
-
-	var conf *bytes.Buffer = new(bytes.Buffer)
-
-	fmt.Fprintf(conf, "Name: %v\n\n", config.Name)
-
-	web, err := yaml.Marshal(config.Web)
+	conf, err := yaml.Marshal(config.RunningConfig)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Fprintf(conf, "Web:\n%v%v\n\n", heading, string(web))
 
-	teams, err := yaml.Marshal(config.Teams)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Fprintf(conf, "Teams:\n%v%v\n\n", heading, string(teams))
-
-	scoring, err := yaml.Marshal(config.Scoring)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Fprintf(conf, "Scoring:\n%v%v\n\n", heading, string(scoring))
-
-	engine, err := yaml.Marshal(config.Engine)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Fprintf(conf, "Engine:\n%v%v\n\n", heading, string(engine))
-
-	checks, err := yaml.Marshal(config.Checks)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Fprintf(conf, "Checks:\n%v%v\n\n", heading, string(checks))
-
-	return &proto.GetConfigResponse{Config: conf.String()}, nil
+	return &proto.GetConfigResponse{Config: string(conf)}, nil
 }
