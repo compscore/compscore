18645fc75838f35f5b360446b9da8fb2aa080d77
1nv8rZim <msf9542@rit.edu>
2023-08-28 21:38:49 -0400

    ent abstraction baseline

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	entgo.io/ent v0.12.3
 	github.com/fsnotify/fsnotify v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.16.0
---
+++ pkg/data/main.go
@@ -0,0 +1,28 @@
+package data
+
+import (
+	"context"
+	"log"
+
+	"github.com/compscore/compscore/pkg/ent"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	Client *ent.Client
+	Ctx    context.Context = context.Background()
+)
+
+func init() {
+	client, err := ent.Open("sqlite3", "file:database.sqlite?_loc=auto&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+
+	Client = client
+
+	// Run the auto migration tool.
+	if err := Client.Schema.Create(Ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
