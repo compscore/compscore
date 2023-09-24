3eab321ff2dc41b0ad1e92a83d9d957fa37638d3
1nv8rZim <msf9542@rit.edu>
2023-09-23 22:40:09 -0400

    Init for data absrtaction to create all teams, checks, and credentials

--- pkg/data/main.go
+++ pkg/data/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/ent"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -24,5 +25,52 @@ func Init() {
 	// Run the auto migration tool.
 	if err := Client.Schema.Create(Ctx); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// Create teams if they do not exist
+	for _, team := range config.Teams {
+		exists, err := Team.Exists(team.Number)
+		if err != nil {
+			log.Fatalf("failed checking for team %d: %v", team.Number, err)
+		}
+
+		if !exists {
+			_, err := Team.Create(team.Number, team.Name, team.Password)
+			if err != nil {
+				log.Fatalf("failed creating team %d: %v", team.Number, err)
+			}
+		}
+	}
+
+	// Create checks if they do not exist
+	for _, check := range config.Checks {
+		exists, err := Check.Exists(check.Name)
+		if err != nil {
+			log.Fatalf("failed checking for check %s: %v", check.Name, err)
+		}
+
+		if !exists {
+			_, err := Check.Create(check.Name)
+			if err != nil {
+				log.Fatalf("failed creating check %s: %v", check.Name, err)
+			}
+		}
+	}
+
+	// Create credentials if they do not exist
+	for _, check := range config.Checks {
+		for _, team := range config.Teams {
+			exists, err := Credential.Exists(team.Number, check.Name)
+			if err != nil {
+				log.Fatalf("failed checking for credential %d:%s: %v", team.Number, check.Name, err)
+			}
+
+			if !exists {
+				_, err := Credential.Create(team.Number, check.Name, team.Password)
+				if err != nil {
+					log.Fatalf("failed creating credential %d:%s: %v", team.Number, check.Name, err)
+				}
+			}
+		}
 	}
 }
