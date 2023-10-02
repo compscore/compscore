bd0a3c39ccb415934689b675f7cade5aabfa6ad0
1nv8rZim <msf9542@rit.edu>
2023-10-01 23:38:28 -0400

    web endpoint for Round Scoreboard

---
+++ pkg/web/roundScoreboard.go
@@ -0,0 +1,25 @@
+package web
+
+import (
+	"strconv"
+
+	"github.com/compscore/compscore/pkg/data"
+	"github.com/gin-gonic/gin"
+)
+
+func roundScoreboard(ctx *gin.Context) {
+	roundStr := ctx.Param("round")
+	round, err := strconv.Atoi(roundStr)
+	if err != nil {
+		ctx.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	scoreboard, err := data.Status.RoundScoreboard(round)
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(200, scoreboard)
+}
