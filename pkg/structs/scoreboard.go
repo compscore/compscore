22320eea9a310e50da848264df08a0bbe45feb4e
1nv8rZim <msf9542@rit.edu>
2023-10-01 21:38:45 -0400

    normalize check structs

--- pkg/structs/scoreboard.go
+++ pkg/structs/scoreboard.go
@@ -1,12 +1,22 @@
 package structs
 
 type Check struct {
-	Name     string `json:"name"`
-	Statuses []int  `json:"teams"`
+	Name   string `json:"name"`
+	Status []int  `json:"status"`
 }
 
 type Scoreboard struct {
 	Round  int     `json:"round"`
 	Scores []int   `json:"scores"`
+	Checks []Check `json:"checks"`
+}
+
+type TeamScoreboard struct {
+	Round  int     `json:"round"`
+	Checks []Check `json:"checks"`
+}
+
+type CheckScoreboard struct {
+	Round  int     `json:"round"`
 	Checks []Check `json:"checks"`
 }
