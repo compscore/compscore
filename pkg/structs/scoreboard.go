1171269225b0f5410f19fb4b11e3ce4e18854733
1nv8rZim <msf9542@rit.edu>
2023-10-02 20:52:14 -0400

    Added structs for Check History

--- pkg/structs/scoreboard.go
+++ pkg/structs/scoreboard.go
@@ -20,3 +20,16 @@ type CheckScoreboard struct {
 	Round int     `json:"round"`
 	Teams []Check `json:"teams"`
 }
+
+type Status struct {
+	Error  string `json:"error"`
+	Time   string `json:"time"`
+	Status int    `json:"status"`
+}
+
+type HistoryStatus struct {
+	Team    string   `json:"team"`
+	Round   int      `json:"round"`
+	Check   string   `json:"check"`
+	History []Status `json:"history"`
+}
