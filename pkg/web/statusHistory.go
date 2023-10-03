0c8eb816d1f76f3fe5796c37ef05dbdfacfac469
1nv8rZim <msf9542@rit.edu>
2023-10-02 21:05:34 -0400

    status history web endpoint

---
+++ pkg/web/statusHistory.go
@@ -0,0 +1,33 @@
+package web
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/compscore/compscore/pkg/data"
+	"github.com/gin-gonic/gin"
+)
+
+func statusHistory(ctx *gin.Context) {
+	check := ctx.Param("check")
+	teamStr := ctx.Param("team")
+
+	team, err := strconv.Atoi(teamStr)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid team"})
+		return
+	}
+
+	if 0 >= team && team >= 127 {
+		ctx.JSON(400, gin.H{"error": "Team number be between 1 and 127"})
+		return
+	}
+
+	statusHistory, err := data.Status.History(check, int8(team), 10)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, statusHistory)
+}
