9f80c75ff0bcbb5c4df15bcfe6c3d22f4e7e7e4f
1nv8rZim <msf9542@rit.edu>
2023-10-08 23:09:12 -0400

    added credential edit endpoint

---
+++ pkg/web/credentialEdit.go
@@ -0,0 +1,62 @@
+package web
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/compscore/compscore/pkg/auth"
+	"github.com/compscore/compscore/pkg/data"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/internal/json"
+)
+
+type credential_edit_s struct {
+	Password string `json:"password"`
+}
+
+func credential_edit(ctx *gin.Context) {
+	var body credential_edit_s
+
+	check := ctx.Param("check")
+
+	entTeam, err := auth.Parse(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	body_bytes, err := io.ReadAll(ctx.Request.Body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = json.Unmarshal(body_bytes, &body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if body.Password == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "No password provided",
+		})
+		return
+	}
+
+	_, err = data.Credential.UpdatePassword(entTeam.Number, check, body.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{})
+}
