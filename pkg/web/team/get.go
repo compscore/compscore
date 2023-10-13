a7c01ad87e6ed630746169bf4eb23887620b39ca
1nv8rZim <msf9542@rit.edu>
2023-10-12 22:10:26 -0400

    team get endpoint

--- pkg/web/team/get.go
+++ pkg/web/team/get.go
@@ -1,9 +1,30 @@
 package team
 
 import (
+	"strconv"
+
+	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
 )
 
 func Get(ctx *gin.Context) {
-	// TODO
+	teamStr := ctx.Param("team")
+
+	team, err := strconv.Atoi(teamStr)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	entTeam, err := data.Team.Get(int8(team))
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, entTeam)
 }
