abc4f9670dfc69e796743879ae69260c9644cdac
1nv8rZim <msf9542@rit.edu>
2023-10-12 22:07:57 -0400

    teams endpoint

--- pkg/web/team/teams.go
+++ pkg/web/team/teams.go
@@ -1,9 +1,18 @@
 package team
 
 import (
+	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
 )
 
 func Teams(ctx *gin.Context) {
-	// TODO
+	entTeams, err := data.Team.GetAll()
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, entTeams)
 }
