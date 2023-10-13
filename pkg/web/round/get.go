a7bcb15945d4e19e86b03454c3ab98d75128c4a6
1nv8rZim <msf9542@rit.edu>
2023-10-12 21:35:08 -0400

    round get endpoint

--- pkg/web/round/get.go
+++ pkg/web/round/get.go
@@ -4,13 +4,15 @@ import (
 	"strconv"
 
 	"github.com/compscore/compscore/pkg/data"
+	"github.com/compscore/compscore/pkg/ent"
+	"github.com/compscore/compscore/pkg/ent/round"
 	"github.com/gin-gonic/gin"
 )
 
 func Get(ctx *gin.Context) {
 	roundStr := ctx.Param("round")
 
-	round, err := strconv.Atoi(roundStr)
+	round_number, err := strconv.Atoi(roundStr)
 	if err != nil {
 		ctx.JSON(500, gin.H{
 			"error": err.Error(),
@@ -18,10 +20,31 @@ func Get(ctx *gin.Context) {
 		return
 	}
 
-	entRound, err := data.Round.GetWithStatus(round)
+	entRound_i, err := data.Client(
+		func(client *ent.Client) (interface{}, error) {
+			return client.Round.Query().
+				WithStatus(
+					func(query *ent.StatusQuery) {
+						query.WithTeam().WithCheck()
+					},
+				).
+				Where(
+					round.NumberEQ(round_number),
+				).
+				Only(ctx)
+		},
+	)
 	if err != nil {
 		ctx.JSON(500, gin.H{
 			"error": err.Error(),
+		})
+		return
+	}
+
+	entRound, ok := entRound_i.(*ent.Round)
+	if !ok {
+		ctx.JSON(500, gin.H{
+			"error": "entRound_i.(*ent.Round) failed",
 		})
 		return
 	}
