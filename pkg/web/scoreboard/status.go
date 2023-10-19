ddc6bba1b0cb79c4db294d1b3e3c0fadd690ee9b
1nv8rZim <msf9542@rit.edu>
2023-10-18 22:50:49 -0400

    built web request

--- pkg/web/scoreboard/status.go
+++ pkg/web/scoreboard/status.go
@@ -26,3 +26,29 @@ func Status(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, statusHistory)
 }
+
+func StatusRound(ctx *gin.Context) {
+	check := ctx.Param("check")
+	teamStr := ctx.Param("team")
+	roundStr := ctx.Param("round")
+
+	team, err := strconv.Atoi(teamStr)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid team"})
+		return
+	}
+
+	round, err := strconv.Atoi(roundStr)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid round"})
+		return
+	}
+
+	statusHistory, err := data.Scoreboard.HistoryRound(check, team, round, 10)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, statusHistory)
+}
