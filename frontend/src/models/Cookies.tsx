c1f4370423605abe7f9bbe8bb6e10777abb39238
1nv8rZim <msf9542@rit.edu>
2023-10-25 22:28:38 -0400

    supressed errors

--- frontend/src/models/Cookies.tsx
+++ frontend/src/models/Cookies.tsx
@@ -1,14 +1,17 @@
 import { CookieSetOptions } from "universal-cookie";
 
 export type cookies = {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   auth?: any;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   admin?: any;
 };
 
 export type setCookie = (
   name: "auth" | "admin",
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   value: any,
   options?: CookieSetOptions | undefined
