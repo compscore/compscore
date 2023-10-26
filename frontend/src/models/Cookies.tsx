62896d3626e9e224b384829c74e7dd501b7d12e4
1nv8rZim <msf9542@rit.edu>
2023-10-25 22:04:17 -0400

    fixed Models/Cookies.tsx

--- frontend/src/models/Cookies.tsx
+++ frontend/src/models/Cookies.tsx
@@ -1,12 +1,15 @@
 import { CookieSetOptions } from "universal-cookie";
 
 export type cookies = {
+  // @ts-ignore
   auth?: any;
+  // @ts-ignore
   admin?: any;
 };
 
 export type setCookie = (
   name: "auth" | "admin",
+  // @ts-ignore
   value: any,
   options?: CookieSetOptions | undefined
 ) => void;
