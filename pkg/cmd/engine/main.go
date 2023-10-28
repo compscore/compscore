b3bf61f9d1a809bddf8c543015d59ab1706bf3be
1nv8rZim <msf9542@rit.edu>
2023-10-28 17:28:22 -0400

    engine command documentation

--- pkg/cmd/engine/main.go
+++ pkg/cmd/engine/main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Cmd is the engine command
 var Cmd = &cobra.Command{
 	Use:     "engine",
 	Aliases: []string{"e", "eng"},
 	Run:     cmdRun,
 }
 
+// cmdRun prints the help for the engine command if no subcommand is specified
 func cmdRun(cmd *cobra.Command, args []string) {
 	err := cmd.Help()
 	if err != nil {
@@ -18,6 +20,7 @@ func cmdRun(cmd *cobra.Command, args []string) {
 	}
 }
 
+// registers all engine commands
 func init() {
 	Cmd.AddCommand(
 		heartbeatCmd,
