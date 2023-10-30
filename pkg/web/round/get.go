dc8480e8ab2353ae6cf00bfadfadd6f0581391c0
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:53:45 -0400

    round.get caching

--- pkg/web/round/get.go
+++ pkg/web/round/get.go
@@ -1,14 +1,33 @@
 package round
 
 import (
+	"fmt"
 	"strconv"
 
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func Get(ctx *gin.Context) {
 	roundStr := ctx.Param("round")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("round/%s", roundStr)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
 
 	round_number, err := strconv.Atoi(roundStr)
 	if err != nil {
@@ -24,6 +43,16 @@ func Get(ctx *gin.Context) {
 			"error": err.Error(),
 		})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("round/%s", roundStr), entRound, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 	}
 
 	ctx.JSON(200, entRound)
