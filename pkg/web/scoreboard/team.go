6a1195426f28ce88bdbc7e5b218a5087bd7d80f3
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:55:16 -0400

    scoreboard.team caching

--- pkg/web/scoreboard/team.go
+++ pkg/web/scoreboard/team.go
@@ -1,14 +1,32 @@
 package scoreboard
 
 import (
+	"fmt"
 	"strconv"
 
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func Team(ctx *gin.Context) {
 	teamStr := ctx.Param("team")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("scoreboard/team/%s", teamStr)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
+
 	team, err := strconv.Atoi(teamStr)
 	if err != nil {
 		ctx.JSON(400, gin.H{"error": err.Error()})
@@ -21,12 +39,33 @@ func Team(ctx *gin.Context) {
 		return
 	}
 
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("scoreboard/team/%s", teamStr), teamScoreboard, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
 	ctx.JSON(200, teamScoreboard)
 }
 
 func TeamRound(ctx *gin.Context) {
 	teamStr := ctx.Param("team")
 	roundStr := ctx.Param("round")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("scoreboard/team/%s/%s", teamStr, roundStr)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
 
 	team, err := strconv.Atoi(teamStr)
 	if err != nil {
@@ -44,6 +83,14 @@ func TeamRound(ctx *gin.Context) {
 	if err != nil {
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("scoreboard/team/%s/%s", teamStr, roundStr), teamScoreboard, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	ctx.JSON(200, teamScoreboard)
