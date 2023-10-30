7faf1168d48507d4e12517a5aaf34072e2bca81a
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:54:55 -0400

    scoreboard.status caching

--- pkg/web/scoreboard/status.go
+++ pkg/web/scoreboard/status.go
@@ -1,16 +1,33 @@
 package scoreboard
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func Status(ctx *gin.Context) {
 	check := ctx.Param("check")
 	teamStr := ctx.Param("team")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("scoreboard/status/%s/%s", check, teamStr)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(http.StatusOK, cachedData)
+			return
+		}
+	}
 
 	team, err := strconv.Atoi(teamStr)
 	if err != nil {
@@ -24,6 +41,14 @@ func Status(ctx *gin.Context) {
 		return
 	}
 
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("scoreboard/status/%s/%s", check, teamStr), statusHistory, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
 	ctx.JSON(http.StatusOK, statusHistory)
 }
 
@@ -31,6 +56,19 @@ func StatusRound(ctx *gin.Context) {
 	check := ctx.Param("check")
 	teamStr := ctx.Param("team")
 	roundStr := ctx.Param("round")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("scoreboard/status/%s/%s/%s", check, teamStr, roundStr)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(http.StatusOK, cachedData)
+			return
+		}
+	}
 
 	team, err := strconv.Atoi(teamStr)
 	if err != nil {
@@ -48,6 +86,14 @@ func StatusRound(ctx *gin.Context) {
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("scoreboard/status/%s/%s/%s", check, teamStr, roundStr), statusHistory, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	ctx.JSON(http.StatusOK, statusHistory)
