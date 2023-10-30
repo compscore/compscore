103d762868871da8372b0e3909375de0286a7aa7
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:57:05 -0400

    status.teamround caching

--- pkg/web/status/getByTeamRound.go
+++ pkg/web/status/getByTeamRound.go
@@ -1,15 +1,34 @@
 package status
 
 import (
+	"fmt"
 	"strconv"
 
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func GetByTeamRound(ctx *gin.Context) {
 	teamStr := ctx.Param("team")
 	roundStr := ctx.Param("round")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("status/team/%s/round/%s", teamStr, roundStr)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
 
 	team, err := strconv.Atoi(teamStr)
 	if err != nil {
@@ -33,6 +52,16 @@ func GetByTeamRound(ctx *gin.Context) {
 			"error": err.Error(),
 		})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("status/team/%s/round/%s", teamStr, roundStr), entStatus, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 	}
 
 	ctx.JSON(200, entStatus)
