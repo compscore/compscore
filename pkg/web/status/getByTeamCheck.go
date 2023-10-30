7a4cbdc485be218a8d1076a08548b4643682f0a8
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:56:53 -0400

    status.teamcheck caching

--- pkg/web/status/getByTeamCheck.go
+++ pkg/web/status/getByTeamCheck.go
@@ -1,15 +1,34 @@
 package status
 
 import (
+	"fmt"
 	"strconv"
 
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func GetByTeamCheck(ctx *gin.Context) {
 	teamStr := ctx.Param("team")
 	check := ctx.Param("check")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("status/team/%s/check/%s", check, teamStr)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
 
 	team, err := strconv.Atoi(teamStr)
 	if err != nil {
@@ -25,6 +44,16 @@ func GetByTeamCheck(ctx *gin.Context) {
 			"error": err.Error(),
 		})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("status/team/%s/check/%s", check, teamStr), entStatus, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 	}
 
 	ctx.JSON(200, entStatus)
