df27252a0235c3a26d0e9bd5b2482d4530f2ffde
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:54:40 -0400

    scoreboard.scoreboard

--- pkg/web/scoreboard/scoreboard.go
+++ pkg/web/scoreboard/scoreboard.go
@@ -1,15 +1,39 @@
 package scoreboard
 
 import (
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func Scoreboard(ctx *gin.Context) {
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, "scoreboard").Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
+
 	scoreboard, err := data.Scoreboard.Main()
 	if err != nil {
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, "scoreboard", scoreboard, config.Redis.MediumRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	ctx.JSON(200, scoreboard)
