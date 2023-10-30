80218c691e7c213b51fd6929b7cfaecbea41feda
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:57:18 -0400

    status.statuses caching

--- pkg/web/status/statuses.go
+++ pkg/web/status/statuses.go
@@ -1,11 +1,29 @@
 package status
 
 import (
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func Statuses(ctx *gin.Context) {
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, "statuses").Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
+
 	entStatuses, err := data.Status.GetAllWithEdges()
 	if err != nil {
 		ctx.JSON(500, gin.H{
