ecaf325b5916f8c76fd6dfd24a8b3c9c5c49a99a
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:57:33 -0400

    team.teams caching

--- pkg/web/team/teams.go
+++ pkg/web/team/teams.go
@@ -1,17 +1,45 @@
 package team
 
 import (
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func Teams(ctx *gin.Context) {
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, "teams").Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
+
 	entTeams, err := data.Team.GetAll()
 	if err != nil {
 		ctx.JSON(500, gin.H{
 			"error": err.Error(),
 		})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, "teams", entTeams, config.Redis.SlowRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 	}
 
 	ctx.JSON(200, entTeams)
