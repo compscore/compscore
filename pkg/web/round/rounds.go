6fd00ae595fd4dca8a4dddcf5a40c6d8850589a4
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:54:09 -0400

    round.rounds caching

--- pkg/web/round/rounds.go
+++ pkg/web/round/rounds.go
@@ -1,17 +1,45 @@
 package round
 
 import (
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func Rounds(ctx *gin.Context) {
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, "rounds").Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
+
 	entRounds, err := data.Round.GetAll()
 	if err != nil {
 		ctx.JSON(500, gin.H{
 			"error": err.Error(),
 		})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, "rounds", entRounds, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 	}
 
 	ctx.JSON(200, entRounds)
