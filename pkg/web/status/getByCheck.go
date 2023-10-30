3c41db0a8ed080979e1fe98ac38b289490a9305c
1nv8rZim <msf9542@rit.edu>
2023-10-30 13:55:42 -0400

    status.check caching

--- pkg/web/status/getByCheck.go
+++ pkg/web/status/getByCheck.go
@@ -1,12 +1,32 @@
 package status
 
 import (
+	"fmt"
+
+	"github.com/compscore/compscore/pkg/cache"
+	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func GetByCheck(ctx *gin.Context) {
 	check := ctx.Param("check")
+
+	if config.Production {
+		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("status/check/%s", check)).Result()
+		if err != nil && err != redis.Nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err == nil {
+			ctx.JSON(200, cachedData)
+			return
+		}
+	}
 
 	entStatus, err := data.Status.GetAllByCheckWithEdges(check)
 	if err != nil {
@@ -14,6 +34,16 @@ func GetByCheck(ctx *gin.Context) {
 			"error": err.Error(),
 		})
 		return
+	}
+
+	if config.Production {
+		err = cache.Client.Set(ctx, fmt.Sprintf("status/check/%s", check), entStatus, config.Redis.FastRefresh).Err()
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 	}
 
 	ctx.JSON(200, entStatus)
