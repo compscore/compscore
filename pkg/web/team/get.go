1ea66806b6ec4e9327e3cf47ebdc74896cae7669
1nv8rZim <msf9542@rit.edu>
2023-11-01 16:28:20 -0400

    team documentation

--- pkg/web/team/get.go
+++ pkg/web/team/get.go
@@ -3,24 +3,40 @@ package team
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"strconv"
 
 	"github.com/compscore/compscore/pkg/cache"
 	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/data"
+	"github.com/compscore/compscore/pkg/web/models"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 )
 
+// Get returns a team
+//
+// @Summary Get a team
+// @Description Get a team
+// @Tags team
+// @Accept json
+// @Produce json
+// @Param team path int true "Team ID"
+// @Success 200 {object} models.Team
+// @Failure 500 {object} models.Error
+// @Router /team/{team} [get]
 func Get(ctx *gin.Context) {
 	teamStr := ctx.Param("team")
 
 	if config.Production {
 		cachedData, err := cache.Client.Get(ctx, fmt.Sprintf("team/%s", teamStr)).Result()
 		if err != nil && err != redis.Nil {
-			ctx.JSON(500, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(
+				http.StatusInternalServerError,
+				models.Error{
+					Error: err.Error(),
+				},
+			)
 			return
 		}
 
@@ -32,34 +48,46 @@ func Get(ctx *gin.Context) {
 
 	team, err := strconv.Atoi(teamStr)
 	if err != nil {
-		ctx.JSON(500, gin.H{
-			"error": err.Error(),
-		})
+		ctx.JSON(
+			http.StatusInternalServerError,
+			models.Error{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
 	entTeam, err := data.Team.Get(team)
 	if err != nil {
-		ctx.JSON(500, gin.H{
-			"error": err.Error(),
-		})
+		ctx.JSON(
+			http.StatusInternalServerError,
+			models.Error{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
 	if config.Production {
 		redisObject, err := json.Marshal(entTeam)
 		if err != nil {
-			ctx.JSON(500, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(
+				http.StatusInternalServerError,
+				models.Error{
+					Error: err.Error(),
+				},
+			)
 			return
 		}
 
 		err = cache.Client.Set(ctx, fmt.Sprintf("team/%s", teamStr), string(redisObject), config.Redis.SlowRefresh).Err()
 		if err != nil {
-			ctx.JSON(500, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(
+				http.StatusInternalServerError,
+				models.Error{
+					Error: err.Error(),
+				},
+			)
 			return
 		}
 	}
