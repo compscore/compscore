cb4d5bcdcc13a45399ccc1981d432b42a4c5da7b
1nv8rZim <msf9542@rit.edu>
2023-11-01 13:27:17 -0400

    documentation of login endpoint

--- pkg/web/login.go
+++ pkg/web/login.go
@@ -11,77 +11,129 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// login_s is the struct used to unmarshal the JSON body of the login request
+// @Summary body of login request
+// @Description body of login request
+// @Tags auth
 type login_s struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
+// cookie_s is the struct used to marshal the JSON response of the login request
+// @Summary response of login request
+// @Description response of login request
+// @Tags auth
+type cookie_s struct {
+	Name       string `json:"name"`
+	Token      string `json:"token"`
+	Expiration int    `json:"expiration"`
+	Path       string `json:"path"`
+	Domain     string `json:"domain"`
+	Secure     bool   `json:"secure"`
+	HttpOnly   bool   `json:"httponly"`
+}
+
+type error_s struct {
+	Error string `json:"error"`
+}
+
+// login authenticates a user and returns a JWT
+//
+// @Summary Authenticate a user
+// @Description Authenticate a user and return a JWT
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param body body login_s true "Username and password"
+// @Success 200 {object} cookie_s
+// @Failure 400
+// @Header 200 {string} Set-Cookie "auth=JWT; Path=/; Domain=hostname; Secure; HttpOnly"
+// @Router /api/login [post]
 func login(ctx *gin.Context) {
 	var body login_s
 
 	body_bytes, err := io.ReadAll(ctx.Request.Body)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+		ctx.JSON(
+			http.StatusBadRequest,
+			error_s{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
 	err = json.Unmarshal(body_bytes, &body)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+		ctx.JSON(
+			http.StatusBadRequest,
+			error_s{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
 	if body.Username == "" {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "No username provided",
-		})
+		ctx.JSON(
+			http.StatusBadRequest,
+			error_s{
+				Error: "no username provided",
+			},
+		)
 		return
 	}
 
 	if body.Password == "" {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "No password provided",
+		ctx.JSON(http.StatusBadRequest, error_s{
+			Error: "no password provided",
 		})
 		return
 	}
 
 	success, err := data.Team.CheckPasswordByName(body.Username, body.Password)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+		ctx.JSON(
+			http.StatusBadRequest,
+			error_s{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
 	if !success {
-		ctx.JSON(400, gin.H{
-			"error": "Invalid username or password",
-		})
+		ctx.JSON(
+			http.StatusBadRequest,
+			error_s{
+				Error: "invalid username or password",
+			},
+		)
 		return
 	}
 
 	token, expiration, err := auth.GenerateJWT(body.Username)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+		ctx.JSON(
+			http.StatusBadRequest,
+			error_s{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
 	ctx.JSON(
 		http.StatusOK,
-		gin.H{
-			"name":       "auth",
-			"token":      token,
-			"expiration": expiration,
-			"path":       "/",
-			"domain":     config.Web.Hostname,
-			"secure":     false,
-			"httponly":   true,
+		cookie_s{
+			Name:       "auth",
+			Token:      token,
+			Expiration: expiration,
+			Path:       "/",
+			Domain:     config.Web.Hostname,
+			Secure:     false,
+			HttpOnly:   true,
 		},
 	)
 }
