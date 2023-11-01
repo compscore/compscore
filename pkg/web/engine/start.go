d5441ef325d54c0a659aa57713f368a3afbe571c
1nv8rZim <msf9542@rit.edu>
2023-11-01 15:58:30 -0400

    engine start docs

--- pkg/web/engine/start.go
+++ pkg/web/engine/start.go
@@ -9,18 +9,41 @@ import (
 	"github.com/compscore/compscore/pkg/config"
 	"github.com/compscore/compscore/pkg/ent/team"
 	"github.com/compscore/compscore/pkg/grpc/client"
+	"github.com/compscore/compscore/pkg/web/models"
 	"github.com/gin-gonic/gin"
 )
 
+// Start starts the engine
+//
+// @Summary Start the engine
+// @Description Start the engine
+// @Tags engine
+// @Accept json
+// @Produce json
+// @Security ServiceAuth
+// @Success 200 {object} models.Status
+// @Failure 401 {object} models.Error
+// @Failure 500 {object} models.Error
+// @Router /engine/start [post]
 func Start(ctx *gin.Context) {
 	entTeam, err := auth.Parse(ctx)
 	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		ctx.JSON(
+			http.StatusUnauthorized,
+			models.Error{
+				Error: "user is not authenticate",
+			},
+		)
 		return
 	}
 
 	if entTeam.Role != team.RoleAdmin {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "not authorized"})
+		ctx.JSON(
+			http.StatusUnauthorized,
+			models.Error{
+				Error: "request requires admin privileges",
+			},
+		)
 		return
 	}
 
@@ -32,9 +55,19 @@ func Start(ctx *gin.Context) {
 
 	message, err := client.Start(engineContext)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		ctx.JSON(
+			http.StatusInternalServerError,
+			models.Error{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"message": message})
+	ctx.JSON(
+		http.StatusOK,
+		models.Status{
+			Message: message,
+		},
+	)
 }
