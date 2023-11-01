db325f72ea43598c43d0665a283b11881bf266eb
1nv8rZim <msf9542@rit.edu>
2023-11-01 15:06:01 -0400

    get check documentation and modeling

--- pkg/web/check/get.go
+++ pkg/web/check/get.go
@@ -1,12 +1,26 @@
 package check
 
 import (
+	"net/http"
+
 	"github.com/compscore/compscore/pkg/auth"
 	"github.com/compscore/compscore/pkg/data"
 	"github.com/compscore/compscore/pkg/ent"
+	"github.com/compscore/compscore/pkg/web/models"
 	"github.com/gin-gonic/gin"
 )
 
+// Get returns a check
+//
+// @Summary Get a check
+// @Description Get a check
+// @Tags check
+// @Accept json
+// @Produce json
+// @Param check path string true "Check ID"
+// @Success 200 {object} models.Check
+// @Failure 500 {object} models.Error
+// @Router /api/checks/{check} [get]
 func Get(ctx *gin.Context) {
 	check := ctx.Param("check")
 
@@ -19,11 +33,13 @@ func Get(ctx *gin.Context) {
 	} else {
 		entCheck, err = data.Check.GetWithTeamCredential(check, entTeam.Number)
 	}
-
 	if err != nil {
-		ctx.JSON(500, gin.H{
-			"error": err.Error(),
-		})
+		ctx.JSON(
+			http.StatusInternalServerError,
+			models.Error{
+				Error: err.Error(),
+			},
+		)
 		return
 	}
 
